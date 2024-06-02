023b821863d0f8de69954f9aff6ddbe000cd4d4a
Thomas McMahon <glassoctopus@yahoo.com>
2024-06-01 22:51:13 -0500

    almost done, but have an error preventing the join

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -3,8 +3,9 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event, Game, Gamer
-
+from levelupapi.models import Event, Game, Gamer, Event_gamer
+from rest_framework.decorators import action
+from rest_framework.exceptions import AuthenticationFailed
 
 class EventView(ViewSet):
     """Level up game types view"""
@@ -19,6 +20,22 @@ class EventView(ViewSet):
         event_game = request.query_params.get('game', None)
         if event_game is not None:
             events = events.filter(game_id=event_game)
+            
+        uid = request.META.get('HTTP_AUTHORIZATION')
+    
+        if uid is None:
+            raise AuthenticationFailed('Authorization header not found')
+
+        try:
+            gamer = Gamer.objects.get(uid=uid)
+        except Gamer.DoesNotExist:
+            raise AuthenticationFailed('Gamer not found')
+
+        for event in events:
+            # Check to see if there is a row in the Event Games table that has the passed in gamer and event
+            event.joined = len(Event_gamer.objects.filter(
+                gamer=gamer, event=event)) > 0
+
         
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
@@ -60,6 +77,43 @@ class EventView(ViewSet):
         game = Event.objects.get(pk=pk)
         game.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(pk=request.data["uid"])
+        event = Event.objects.get(pk=pk)
+        attendee = Event_gamer.objects.create(
+            gamer=gamer,
+            event=event
+        )
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+    
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Delete request for a user to sign out of an event"""
+        try:
+            # Ensure the uid is being accessed correctly
+            if 'uid' not in request.data:
+                return Response({'error': 'uid is required'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            gamer = Gamer.objects.get(pk=request.data["uid"])
+            event = Event.objects.get(pk=pk)
+            attendee = Event_gamer.objects.get(gamer=gamer, event=event)
+            attendee.delete()
+            return Response({'message': 'Gamer removed from event'}, status=status.HTTP_204_NO_CONTENT)
+        except Gamer.DoesNotExist:
+            return Response({'error': 'Gamer not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Event.DoesNotExist:
+            return Response({'error': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Event_gamer.DoesNotExist:
+            return Response({'error': 'Gamer is not signed up for this event'}, status=status.HTTP_404_NOT_FOUND)
+        except KeyError as e:
+            return Response({'error': f'Missing field: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games"""
@@ -79,4 +133,4 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ('game', 'description', 'date', 'time', 'organizer', 'id')
+        fields = ('game', 'description', 'date', 'time', 'organizer', 'id', 'joined')
